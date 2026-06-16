ca6fed7bb006ae13ad13023581cfbc713f6728e7
Sachin Kumar <sachinkumar22018472@gmail.com>
2026-06-16 18:03:33 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ const port = process.env.PORT || 5000
 
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://chatapplication-96ig.onrender.com",
     credentials: true
 }))
 
