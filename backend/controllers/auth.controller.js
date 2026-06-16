cd765526a8e5f6e101c0a75800f36785e6834ad1
Sachin Kumar <sachinkumar22018472@gmail.com>
2026-06-16 19:09:15 +0530

    Update auth.controller.js

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -35,8 +35,8 @@ export const signUp = async (req, res) => {
         res.cookie("token", token, {
             httpOnly: true,
             maxAge: 7*24*60*60*1000,
-            sameSite: "Strict",
-            secure: false
+            sameSite: "None",
+            secure: true
         })
 
         return res.status(201).json(user)
